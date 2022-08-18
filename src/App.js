32771dbda69e43094209e105cfed44e226f2eeda
Pakaporn24 <110577837+Pakaporn24@users.noreply.github.com>
2022-08-18 23:18:06 +0700

    part7

--- src/App.js
+++ src/App.js
@@ -3,13 +3,41 @@ import './App.css';
 import WordCard from './WordCard';
 
 const word = "Hello";
-function App() {
+const animal = "elephant";
+const animal1 = "Dog";
+const animal2 = "cat";
+const animal3 = "pig";
+const car = "car";
+const umbrella = "umbrella";
+const pen = "pen";
 
+function App() {
   return (
     <div>
+      <div class="cities">
+      <header>
+      <h1>Word Game</h1>
+      </header>
+      <p>~~~~~~~~~~~~~~~~~~~~~~~</p>
+      
+      <button type="button"
+      onclick="document.getElementById('test').innerHTML = Date()">
+        Spell the words correctly.
+        
+        </button>
+          </div>
+        <h2>Category</h2>
         <WordCard value={word}/>
+        <h2>Animal</h2>
+        <WordCard value={animal}/>
+        <WordCard value={animal1}/>
+        <WordCard value={animal2}/>
+        <h2>Consumables</h2>
+        <WordCard value={car}/>
+        <WordCard value={umbrella}/>
+        <WordCard value={pen}/>
+
     </div>
       );
-
    }
   export default App;
