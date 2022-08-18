e8dc551077ffe4a119dac5c3dfaf71f19c7db381
Pakaporn24 <110577837+Pakaporn24@users.noreply.github.com>
2022-08-18 11:38:42 +0700

    part 4

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import React from 'react';
 import './App.css';
-import WordCard from './WordCard';
 
 const word = "Hello"
 function App() {
   return (
-<<<<<<< HEAD
     <div>
-      <WordCard value={word}/>
-=======
-    <div className="App">
      Hello World
 >>>>>>> e52036e38aa53ffacd5c2cb93e10bb90dd1f797a
     </div>
